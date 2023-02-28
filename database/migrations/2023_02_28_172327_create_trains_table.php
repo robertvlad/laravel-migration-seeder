4e1ef4a80addf87f3402901496a57b3f62ba2441
robertvlad <vladrob96@gmail.com>
2023-02-28 19:47:11 +0100

    Prima migration lanciata con tabelle annesse

--- database/migrations/2023_02_28_172327_create_trains_table.php
+++ database/migrations/2023_02_28_172327_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza', 100);
+            $table->string('stazione_di_arrivo', 100);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno', 10);
+            $table->tinyInteger('numero_carroze')->unsigned();
+            $table->tinyInteger('in_orario')->unsigned()->default('1');
+            $table->tinyInteger('cancellato')->unsigned()->default('0');
             $table->timestamps();
         });
     }
